bf7adb72fd543a11a08fff18b3efe50fe60fd301
kimothokamau <kimothokamau@gmail.com>
2021-09-14 11:28:06 -0600

    Add profile page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
