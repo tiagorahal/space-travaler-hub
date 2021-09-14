6c7ffa3b1e25870d18dbc357fea2cce06069ab42
kimothokamau <kimothokamau@gmail.com>
2021-09-14 12:09:03 -0600

    Create rocket and modify rockets to display props

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,10 +1,26 @@
+import react from 'react';
+import { useSelector } from 'react-redux';
+import Rocket from './Rocket';
 import rocketHooks from '../hooks/rockets';
 
 const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
   rocketHooks();
 
   return (
-    <h1>Rcokets from hooks API</h1>
+    <div>
+      {
+        rockets.map((rocket) => 
+          <Rocket
+            key={rocket.id}
+            id={rocket.id}
+            name={rocket.name}
+            description={rocket.description}
+            image={rocket.imageUrl}
+          />
+        )
+      }
+    </div>
   );
 };
 
