a66679fdd376f5ad3c2d7ae7e88c4e71aa9ce353
kimothokamau <kimothokamau@gmail.com>
2021-09-14 12:04:42 -0600

    Modify rockets component to diplay api response

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,3 +1,11 @@
-const Rockets = () => (<h1>Rockets</h1>);
+import rocketHooks from '../hooks/rockets';
+
+const Rockets = () => {
+  rocketHooks();
+
+  return (
+    <h1>Rcokets from hooks API</h1>
+  );
+};
 
 export default Rockets;
