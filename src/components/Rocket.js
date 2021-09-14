c598c91c58c0e6bd47c21439109efc55cfe08744
kimothokamau <kimothokamau@gmail.com>
2021-09-14 12:14:15 -0600

    Add badge and button components

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { reserveRocketAction } from '../redux/rockets/rockets';
-import Button from './Button';
-import Badge from './Badge';
+// import { reserveRocketAction } from '../redux/rockets/rockets';
+// import Button from './Button';
+// import Badge from './Badge';
 
 const Rocket = ({
   id, name, description, image, reserved,
