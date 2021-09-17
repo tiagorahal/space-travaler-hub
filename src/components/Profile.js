a7a7032ece1f6ea90767bf6462c6b0f2eb758d39
kimothokamau <kimothokamau@gmail.com>
2021-09-17 00:46:38 -0600

    Add profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,38 @@
-const Profile = () => (<h1>Profile</h1>);
+import {
+  Table, Container, Col, Row,
+} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+const Profile = (props) => {
+  const { rockets } = props;
+  const activeRockets = rockets.filter((rocket) => rocket.reserved === true);
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>My Missions</h2>
+        </Col>
+        <Col>
+          <h2>My Rockets</h2>
+          <Table bordered>
+            <tbody>
+              {activeRockets.map((rocket) => (
+                <tr key={rocket.id}>
+                  <td>
+                    {rocket.name}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+Profile.propTypes = {
+  rockets: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
 
 export default Profile;
