15814cab031f5689a353146a32f2796a9dbdf4ed
kimothokamau <kimothokamau@gmail.com>
2021-09-17 08:25:38 -0600

    Fix rockets re-render, hooks, cancel reservation

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import Header from './components/Header';
 import Profile from './components/Profile';
 import Rockets from './components/Rockets';
 import Missions from './components/missions';
+import rocketHooks from './hooks/rockets';
 
 function App() {
   const rockets = useSelector((state) => state.rocketsReducer);
+
+  rocketHooks();
+
   return (
     <div className="App">
       <Router>
